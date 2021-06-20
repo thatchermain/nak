06715f43b956e4f373eab474777f298089af9639
Dominik Krzyształowicz <drive.grid@gmail.com>
2021-06-20 10:55:26 +0200

    Reworked home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ac from '../images/ac_white.png';
-import repair from '../images/repair_white.png';
-import check from '../images/check_white.png';
 import wrench from '../images/wrench.png';
 import './Home.css';
-import Card from '../components/Card';
+import Typical from 'react-typical';
+
+// import ac from '../images/ac_white.png';
+// import repair from '../images/repair_white.png';
+// import check from '../images/check_white.png';
+// import Card from '../components/Card';
+
 
 
 const Home = () => {
@@ -16,14 +19,27 @@ const Home = () => {
             <div className="main">
                 <div className="title">
                     <div className="title__services">
-                        <h3 className='title__service'><img className='title__icon' src={wrench} alt="" />Stacja kontroli pojazdów</h3>
-                        <h3 className='title__service'><img className='title__icon' src={wrench} alt="" />Klimatyzacja</h3>
-                        <h3 className='title__service'><img className='title__icon' src={wrench} alt="" />Mechanika pojazdowa</h3>
+                        <h3 className='title__service'>
+                        <Typical 
+                            loop={Infinity}
+                            wrapper='strong'
+                            steps={[
+                                '📋 Stacja kontroli pojazdów',
+                                1000,
+                                '❄️ Klimatyzacja',
+                                1000,
+                                '🔧 Mechanika pojazdowa',
+                                1000
+                            ]}
+                        />
+                        </h3>
+                        {/* <h3 className='title__service'><img className='title__icon' src={wrench} alt="" />Klimatyzacja</h3>
+                        <h3 className='title__service'><img className='title__icon' src={wrench} alt="" />Mechanika pojazdowa</h3> */}
                     </div>
 
                     {/* <h2 className='title__bottom'>Zapraszamy do zapoznania się z naszą ofertą.</h2> */}
                 </div>
-                <div className="info">
+                {/* <div className="info">
                     <div className="info__phone">
                         <h2>Masz pytanie? Zadzwoń!</h2>
                         <h2><span className='info__span'>123-465-789</span></h2>
@@ -35,7 +51,7 @@ const Home = () => {
                         <h3><span className='info__span'>33-100 Tarnów</span></h3>
                     </div>
 
-                </div>
+                </div> */}
             </div>
         </div>
     );
